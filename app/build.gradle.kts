11f7e81fad503ddddc22343bcf504b21cd16cffa
hilt <ngoctan9102000@gmail.com>
2025-04-26 17:28:37 +0700

    add library

---
+++ app/build.gradle.kts
@@ -0,0 +1,91 @@
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.com.google.devtools.ksp)
+    alias(libs.plugins.com.google.dagger.hilt.android)
+}
+
+android {
+    namespace = "ngoctan.traininng.androidproject"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "ngoctan.traininng.androidproject"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+    kotlinOptions {
+        jvmTarget = "21"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    // Timber
+    implementation(libs.com.jakewharton.timber)
+    // Moshi
+    implementation(libs.com.squareup.moshi.kotlin)
+    implementation(libs.com.squareup.retrofit2.converter.moshi)
+    // Gson
+    implementation(libs.com.google.code.gson)
+    // Coroutine
+    implementation(libs.org.jetbrains.kotlinx.coroutines)
+    // OkHttp
+    implementation(libs.com.squareup.okhttp)
+    implementation(libs.com.squareup.okhttp3.logging.interceptor)
+    // Room database
+    implementation(libs.androidx.runtime.room)
+    ksp(libs.androidx.room.compiler)
+    implementation (libs.androidx.room.ktx)
+
+    // life cycle
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.savedstate)
+
+    // Retrofit
+    implementation (libs.com.squareup.retrofit)
+    implementation (libs.com.squareup.retrofit.converter.gson)
+    // Skydives
+    implementation (libs.com.github.skydoves.sandwich)
+    // Hilt
+    implementation(libs.dagger.hilt)
+    ksp(libs.dagger.hilt.android.compiler)
+    // navigation
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+    //coil
+    implementation(libs.coil)
+}
